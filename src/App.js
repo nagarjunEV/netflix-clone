acf0295e6bce56b9b44d634b30084b1df5598a9d
Nagarjun Enumudi Venkatesha <nagarjunev.11@gmail.com>
2022-03-20 21:55:17 -0500

    Added different genre movie rows

--- src/App.js
+++ src/App.js
@@ -9,7 +9,17 @@ function App() {
         title="Netflix Orginials"
         fetchUrl={urls.fetchNetflixOriginals}
       ></Row>
-      <Row title="Trending" fetchUrl={urls.fetchTrending}></Row>
+      <Row
+        title="Trending"
+        fetchUrl={urls.fetchTrending}
+        isLargeRow={true}
+      ></Row>
+      <Row title="Top Rated" fetchUrl={urls.fetchTopRated}></Row>
+      <Row title="Action" fetchUrl={urls.fetchActionMovies}></Row>
+      <Row title="Comedy" fetchUrl={urls.fetchComedyMovies}></Row>
+      <Row title="Horror" fetchUrl={urls.fetchHorrorMovies}></Row>
+      <Row title="Romance" fetchUrl={urls.fetchRomanceMovies}></Row>
+      <Row title="Documantaries" fetchUrl={urls.fetchDocumantaries}></Row>
     </div>
   );
 }
--- src/reusable/Row.js
+++ src/reusable/Row.js
@@ -1,23 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import axios from './../api/baseUrl';
+import './row.css';
 
-const Row = ({ title, fetchUrl }) => {
+const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [moviesList, setMoviesList] = useState([]);
-
+  const imgUrl = `https://image.tmdb.org/t/p/original/`;
   useEffect(() => {
-    console.log(`useEffect Row`);
+    // console.log(`useEffect Row`);
     async function fetechData() {
       const response = await axios.get(fetchUrl);
-      console.log(`Reposne for ${fetchUrl} is ${response.data.results}`);
+      // console.log(`${response.data.results}`);
+      setMoviesList(response.data.results);
     }
     fetechData();
   }, [fetchUrl]);
 
   return (
-    <div>
+    <div className="row">
       {/* Title */}
       <h2>{title}</h2>
       {/* Cards for Movies */}
+      <div className="row-posters">
+        {moviesList.map((movie, i) => (
+          <img
+            key={movie.name}
+            className={`row-poster ${isLargeRow && 'row-poster-large'}`}
+            src={`${imgUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 };
